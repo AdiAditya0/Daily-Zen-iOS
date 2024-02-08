e334acee59ac9c8672bb09b6c2c4d75180649739
akumrawat <akumrawat@deqode.com>
2024-02-08 18:40:41 +0530

    Clean-up setup

--- Daily Zen/Daily_ZenApp.swift
+++ Daily Zen/Daily_ZenApp.swift
@@ -1,20 +1,10 @@
-//
-//  Daily_ZenApp.swift
-//  Daily Zen
-//
-//  Created by Aditya Kumrawat on 08/02/24.
-//
-
 import SwiftUI
 
 @main
 struct Daily_ZenApp: App {
-    let persistenceController = PersistenceController.shared
-
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            RootNavigation()
         }
     }
 }
---
+++ Daily Zen/Features/RootNavigation/View/RootNavigation.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct RootNavigation: View {
+    var body: some View {
+        NavigationStack {
+            VStack{}
+        }
+    }
+}
+
+struct RootNavigation_Previews: PreviewProvider {
+    static var previews: some View {
+        RootNavigation()
+    }
+}
